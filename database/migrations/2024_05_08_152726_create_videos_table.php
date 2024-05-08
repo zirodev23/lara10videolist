ba71f9e86ee09f6ce03102d3cac2f0d335a9e897
zirodev <zirodev23@zirodev23.com>
2024-05-08 19:50:44 +0300

    attach playlist and video to users; private or public they all must have owner

--- database/migrations/2024_05_08_152726_create_videos_table.php
+++ database/migrations/2024_05_08_152726_create_videos_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('videos', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->foreignId('user_id')->constrained('users');
             $table->timestamps();
         });
     }
