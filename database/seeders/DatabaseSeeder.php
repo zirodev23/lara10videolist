e9284aeabdddc6e9c54619da2ee82a42592483a0
zirodev <zirodev23@zirodev23.com>
2024-05-08 19:27:48 +0300

    user with id 1 seeded; php artisan migrate:refresh --seed; !!! all data will be lost

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(1)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'id' => 1,
+            'name' => 'Roberts',
+            'email' => 'skilled.user@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => \Str::random(10),
+        ]);
     }
 }
